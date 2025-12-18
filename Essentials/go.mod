c2a9f12381519e6d5057b503e353465e28a90305
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2025-12-18 14:40:29 +0200

    first commit

---
+++ Essentials/go.mod
@@ -0,0 +1,3 @@
+module Day-01
+
+go 1.25
---
+++ Essentials/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	revenue, err := getUserInput("Revenue: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	expenses, err := getUserInput("Expenses: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	taxRate, err := getUserInput("Tax Rate: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+
+	fmt.Printf("EBT: %.1f\n", ebt)
+	fmt.Printf("Profit: %.1f\n", profit)
+	fmt.Printf("Ratio: %.3f\n", ratio)
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
+}
+
+func getUserInput(infoText string) (float64, error) {
+
+	var userInput float64
+	fmt.Print(infoText)
+
+	fmt.Scan(&userInput)
+
+	if userInput <= 0 {
+		return 0, errors.New("Value must be greater than 0")
+	}
+
+	return userInput, nil
+}
